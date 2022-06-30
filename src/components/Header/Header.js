8796fe8f4fd5006c4174672fe4aae520aa8d0ff2
danielortegaperez <ortegaperezdaniel@gmail.com>
2022-06-30 13:23:08 +0200

    changes in css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,59 +5,62 @@ import { useToken } from '../../TokenContext';
 import './Header.css';
 
 const Header = () => {
-    const [token, setToken] = useToken();
-    const [username, setUsername] = useState();
+  const [token, setToken] = useToken();
+  const [username, setUsername] = useState();
 
-    const userData = async () => {
-        try {
-            const res = await fetch('http://localhost:4000/users', {
-                headers: {
-                    Authorization: token,
-                },
-            });
+  const userData = async () => {
+    try {
+      const res = await fetch('http://localhost:4000/users', {
+        headers: {
+          Authorization: token,
+        },
+      });
 
-            const body = await res.json();
+      const body = await res.json();
 
-            if (body.status === 'ok') {
-                setUsername(body.data.user.username);
-            }
-        } catch (err) {
-            console.error(err);
-        }
-    };
+      if (body.status === 'ok') {
+        setUsername(body.data.user.username);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    if (token) userData();
+  if (token) userData();
 
-    return (
-        <header>
-            <h1>
-                <NavLink to='/'>Mini Twitter</NavLink>
-            </h1>
-            <div className='Buttons'>
-                {token && <p>@{username}</p>}
-                {!token && (
-                    <div className='Button'>
-                        <NavLink to='/login'>Log In</NavLink>
-                    </div>
-                )}
-                {!token && (
-                    <div className='Button'>
-                        <NavLink to='/signup'>Sign Up</NavLink>
-                    </div>
-                )}
-                {token && (
-                    <div className='Button'>
-                        <NavLink to='/new'>Nuevo</NavLink>
-                    </div>
-                )}
-                {token && (
-                    <div className='Button' onClick={() => setToken(null)}>
-                        <p>Logout</p>
-                    </div>
-                )}
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <h1>
+        <p id='Cabecera'>Gimnasio Los Chapuceros</p>
+        <NavLink to='/'>
+          <img src='/logo.jpg' alt='logo' id='Logo' />
+        </NavLink>
+      </h1>
+      <div className='Buttons'>
+        {token && <p>@{username}</p>}
+        {!token && (
+          <div className='Button'>
+            <NavLink to='/login'>Iniciar sesión</NavLink>
+          </div>
+        )}
+        {!token && (
+          <div className='Button'>
+            <NavLink to='/signup'>Registrarse</NavLink>
+          </div>
+        )}
+        {token && (
+          <div className='Button'>
+            <NavLink to='/new'>Nuevo ejercicio</NavLink>
+          </div>
+        )}
+        {token && (
+          <div className='Button' onClick={() => setToken(null)}>
+            <p>Salir de sesión</p>
+          </div>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
